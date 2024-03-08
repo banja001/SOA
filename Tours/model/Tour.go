37bfabc53e2d99425961082240c2bb8093fe8715
Ana <anaranic01@gmail.com>
2024-03-08 17:53:33 +0100

    fix: Fixed Tour model

--- Tours/model/Tour.go
+++ Tours/model/Tour.go
@@ -26,21 +26,21 @@ const (
 )
 
 type Tour struct {
-	ID            uuid.UUID      `json:"id" gorm:"primary_key"`
-	Name          string         `json:"name" gorm:"not null;type:string"`
-	Description   string         `json:"description" gorm:"not null;type:string"`
-	Difficulty    TourDifficulty `json:"difficulty" gorm:"type:enum('Beginner', 'Intermediate', 'Advanced', 'Pro')"`
-	Tags          []string       `json:"tags" gorm:"not null;type:string"`
-	Status        TourStatus     `json:"status" gorm:"type:enum('Draft', 'Published', 'Archived', 'TouristMade')"`
-	Price         float64        `json:"price"`
-	AuthorId      int            `json:"authorId"`
-	Equipment     []int          `json:"equipment" gorm:"type:string"`
-	DistanceInKm  float64        `json:"distanceInKm"`
-	ArchivedDate  *time.Time     `json:"archivedDate"`
-	PublishedDate *time.Time     `json:"publishedDate"`
+	ID          uuid.UUID      `json:"Id" gorm:"primary_key"`
+	Name        string         `json:"Name" gorm:"not null;type:string"`
+	Description string         `json:"Description" gorm:"not null;type:string"`
+	Difficulty  TourDifficulty `json:"Difficulty" gorm:"type:string"`
+	//Tags []string `json:"Tags" gorm:"not null;type:text"`
+	Status   TourStatus `json:"Status" gorm:"type:string"`
+	Price    float64    `json:"Price"`
+	AuthorId int        `json:"AuthorId"`
+	//Equipment     []int      `json:"Equipment" gorm:"type:integer[]"`
+	DistanceInKm  float64    `json:"DistanceInKm"`
+	ArchivedDate  *time.Time `json:"ArchivedDate"`  //?
+	PublishedDate *time.Time `json:"PublishedDate"` //?
 	//Durations     []TourDuration `gorm:"foreignKey:TourID"`
 	//KeyPoints     []TourKeyPoint `gorm:"foreignKey:TourID"`
-	Image string `json:"image" gorm:"not null;type:string"`
+	Image string `json:"Image" gorm:"type:string"` //?
 }
 
 func (tour *Tour) BeforeCreate(scope *gorm.DB) error {
