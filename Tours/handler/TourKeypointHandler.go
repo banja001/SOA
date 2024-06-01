762dde0562e9b7c293903e1c9df06c8e69a5a9ed
Ana <anaranic01@gmail.com>
2024-06-01 14:16:05 +0200

    fix: Fixed GetTourKeypointById

--- Tours/handler/TourKeypointHandler.go
+++ Tours/handler/TourKeypointHandler.go
@@ -7,6 +7,7 @@ import (
 	"database-example/service"
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 type TourKeypointHandler struct {
@@ -15,18 +16,19 @@ type TourKeypointHandler struct {
 }
 
 func (handler *TourKeypointHandler) Get(ctx context.Context, req *pb.TourKeypointIdRequest) (*pb.TourKeypoint, error) {
-	tourKeypoint, err := handler.TourKeypointService.Find(string(req.Id))
+	tourKeypoint, err := handler.TourKeypointService.Find(strconv.Itoa(int(req.Id)))
+
 	if err != nil {
 		return nil, fmt.Errorf("tour keypoint id %d not found", req.Id)
 	}
 	return modelToProto(tourKeypoint), nil
 }
 
-func (handler *TourKeypointHandler) Create(ctx context.Context, req *pb.TourKeypoint) (*pb.TourKeypoint, error){
+func (handler *TourKeypointHandler) Create(ctx context.Context, req *pb.TourKeypoint) (*pb.TourKeypoint, error) {
 	if req == nil {
 		return nil, fmt.Errorf("request or tour keypoint data is nil")
 	}
-	
+
 	tourKeypoint := protoToModel(req)
 
 	if tourKeypoint == nil {
@@ -42,11 +44,11 @@ func (handler *TourKeypointHandler) Create(ctx context.Context, req *pb.TourKeyp
 	return modelToProto(createdTourKeypoint), nil
 }
 
-func (handler *TourKeypointHandler) Update(ctx context.Context, req *pb.TourKeypoint) (*pb.TourKeypoint, error){
+func (handler *TourKeypointHandler) Update(ctx context.Context, req *pb.TourKeypoint) (*pb.TourKeypoint, error) {
 	if req == nil {
 		return nil, fmt.Errorf("request or tour keypoint data is nil")
 	}
-	
+
 	tourKeypoint := protoToModel(req)
 	if tourKeypoint == nil {
 		return nil, errors.New("Error while parsing tour keypoint! (update)")
@@ -58,43 +60,43 @@ func (handler *TourKeypointHandler) Update(ctx context.Context, req *pb.TourKeyp
 	return modelToProto(updatedTourKeypoint), nil
 }
 
-func (handler *TourKeypointHandler) Delete(ctx context.Context, req *pb.TourKeypointIdRequest) (*pb.EmptyResponse, error)  {
+func (handler *TourKeypointHandler) Delete(ctx context.Context, req *pb.TourKeypointIdRequest) (*pb.EmptyResponse, error) {
 
-	err := handler.TourKeypointService.Delete(string(req.GetId()))
+	err := handler.TourKeypointService.Delete(strconv.Itoa(int(req.Id)))
 	if err != nil {
 		return nil, err
 	}
 	return &pb.EmptyResponse{}, nil
 }
 
-func modelToProto(model *model.TourKeypoint)*pb.TourKeypoint {
+func modelToProto(model *model.TourKeypoint) *pb.TourKeypoint {
 
-    return &pb.TourKeypoint{
-        Id:               int32(model.ID),
-        Name:             string(model.Name),
-        Description:	  string(model.Description),
-        Image:            string(model.Image),
-		Latitude:         float64(model.Latitude),
-		Longitude:        float64(model.Longitude),
-		TourId:           int32(model.TourID),
-		Secret:           string(model.Secret),
-		PositionInTour:   int32(model.PositionInTour),
-		PublicPointId:    int32(model.PublicPointID),
-    }
+	return &pb.TourKeypoint{
+		Id:             int32(model.ID),
+		Name:           string(model.Name),
+		Description:    string(model.Description),
+		Image:          string(model.Image),
+		Latitude:       float64(model.Latitude),
+		Longitude:      float64(model.Longitude),
+		TourId:         int32(model.TourID),
+		Secret:         string(model.Secret),
+		PositionInTour: int32(model.PositionInTour),
+		PublicPointId:  int32(model.PublicPointID),
+	}
 }
 
-func protoToModel(req *pb.TourKeypoint)*model.TourKeypoint {
+func protoToModel(req *pb.TourKeypoint) *model.TourKeypoint {
 
-    return &model.TourKeypoint{
-        ID:             int(req.GetId()),
-        Name:           req.GetName(),
-        Description:    req.GetDescription(),
-        Image:          req.GetImage(),
-        Latitude:       float64(req.GetLatitude()),
-        Longitude:      float64(req.GetLongitude()),
-        TourID:         int(req.GetTourId()),
-        Secret:         req.GetSecret(),
-        PositionInTour: int(req.GetPositionInTour()),
-        PublicPointID:  int(req.GetPublicPointId()),
-    }
+	return &model.TourKeypoint{
+		ID:             int(req.GetId()),
+		Name:           req.GetName(),
+		Description:    req.GetDescription(),
+		Image:          req.GetImage(),
+		Latitude:       float64(req.GetLatitude()),
+		Longitude:      float64(req.GetLongitude()),
+		TourID:         int(req.GetTourId()),
+		Secret:         req.GetSecret(),
+		PositionInTour: int(req.GetPositionInTour()),
+		PublicPointID:  int(req.GetPublicPointId()),
+	}
 }
