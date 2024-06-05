fc6a0bd3f69c0a62071ab66294fbb8ddc6abb1da
Jelena <jblanusa13@gmail.com>
2024-06-05 20:12:12 +0200

    feat: Finished tracing for api gateway

--- Tours/main.go
+++ Tours/main.go
@@ -21,10 +21,65 @@ import (
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 )
+
+const serviceName = "go-tour-service"
+
+var tp *trace.TracerProvider
+
+func initTracer() (*trace.TracerProvider, error) {
+	// Ukoliko je definisana JAEGER_ENDPOINT env var, intanciraj JagerTracer koji šalje trace-ove Jaeger-u,
+	// u suprotnom instanciraj FileTracer koji upisuje trace-ove u json fajl
+	url := os.Getenv("JAEGER_ENDPOINT")
+	if len(url) > 0 {
+		return initJaegerTracer(url)
+	} else {
+		return initFileTracer()
+	}
+}
+
+func initFileTracer() (*trace.TracerProvider, error) {
+	log.Println("Initializing tracing to traces.json")
+	f, err := os.Create("traces.json")
+	if err != nil {
+		return nil, err
+	}
+	exporter, err := stdouttrace.New(
+		stdouttrace.WithWriter(f),
+		stdouttrace.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithSampler(trace.AlwaysSample()),
+	), nil
+}
+
+func initJaegerTracer(url string) (*trace.TracerProvider, error) {
+	log.Printf("Initializing tracing to Jaeger for service: %s at %s\n", serviceName, url)
+	log.Printf("Initializing tracing to jaeger at %s\n", url)
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	return trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		)),
+	), nil
+}
 
 func GetConnectionString() string {
 
